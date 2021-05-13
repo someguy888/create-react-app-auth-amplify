c80b2401511fec3772e0bcc22c90022fc7c11828
someguy888 <84056043+someguy888@users.noreply.github.com>
2021-05-12 22:05:43 -0400

    modified something

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn to React ...
           </a>
         </header>
       </div>
